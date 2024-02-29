5bfec67ff11c0bdde2c7f6cb912b042670a51bfd
Alessandro <alessandrobruno199@gmail.com>
2024-02-29 10:16:09 +0100

    classi

--- Csharp/Programmi/prog4_Indovinello/Vers2_Classi/Program.cs
+++ Csharp/Programmi/prog4_Indovinello/Vers2_Classi/Program.cs
@@ -3,62 +3,107 @@
     public string? nome;
     public int punteggio;
 
+    public int numeroOttenuto;
+
     public Giocatore(string nome, int punteggio)
     {
         this.nome = nome;
         this.punteggio = punteggio;
     }
 
+
     public void Stampa()
     {
-        Console.WriteLine($"\nG1: {nome} Punteggio: {punteggio}");
+        Console.WriteLine($"{nome} Punteggio: {punteggio}");
     }
+
+
 }
 
 
 class Partita
 {
+
+
     public void Gioca()
     {
 
-        Random random = new Random();
         Giocatore g1 = new Giocatore("Alex", 0);
         Giocatore g2 = new Giocatore("Simo", 0);
 
-        while (true)
-        {
-
-            int risG1 = random.Next(1, 7);
-            int risG2 = random.Next(1, 7);
-
-            if (risG1 > risG2)
-            {
-                Console.WriteLine($"Vince {g1.nome}");
-                g1.punteggio++;
-            }
-            else if (risG2 > risG1)
-            {
-                Console.WriteLine($"Vince {g2.nome}");
-                g2.punteggio++;
-            }
-            else
-            {
-                Console.WriteLine("Pareggio");
-            }
-
-            Console.Write("continuare? (s/n): ");
-            string risp = Console.ReadLine()!;
-            if (risp == "n" || risp == "N")
-            {
-                break;
-            }
-
-        }
+        Round(g1, g2);
 
         g1.Stampa();
         g2.Stampa();
     }
 
+
+
+    public void Round(Giocatore g1, Giocatore g2)
+    {
+        int copia1 = 0, copia2 = 0;
+
+        while (true)
+        {
+
+
+            Random random = new Random();
+            g1.numeroOttenuto = random.Next(1, 7);
+            g2.numeroOttenuto = random.Next(1, 7);
+
+
+            Console.Write($"\n{g1.nome}:{g1.numeroOttenuto} - {g2.nome}:{g2.numeroOttenuto}  ---  ");
+
+            if (g1.numeroOttenuto == copia1)
+            {
+                Console.Write($"\n{g1.nome} ha ottenuto due volte di seguito lo stesso numero: -2 punti;");
+                g1.punteggio -= 2;
+            }
+            else if (g2.numeroOttenuto == copia2)
+            {
+                Console.Write($"\n{g2.nome} ha ottenuto due volte di seguito lo stesso numero: -2 punti;");
+                g2.punteggio -= 2;
+            }
+
+            else
+            {
+
+                if (g1.numeroOttenuto > g2.numeroOttenuto)
+                {
+                    Console.WriteLine($"Vince {g1.nome}");
+                    g1.punteggio++;
+                }
+                else if (g2.numeroOttenuto > g1.numeroOttenuto)
+                {
+                    Console.WriteLine($"Vince {g2.nome}");
+                    g2.punteggio++;
+                }
+                else
+                {
+                    Console.WriteLine("Pareggio");
+                }
+
+
+            }
+
+            copia1 = g1.numeroOttenuto;
+            copia2 = g2.numeroOttenuto;
+
+
+
+            Console.Write("\ncontinuare? (s/n): ");
+            string risp = Console.ReadLine()!;
+            if (risp == "n" || risp == "N")
+            {
+                Console.WriteLine("\n");
+                break;
+            }
+
+
+        }
+
+
+    }
 }
 
 
@@ -67,6 +112,7 @@ class Program
 
     public static void Main(string[] args)
     {
+        Console.Clear();
         Partita partita = new Partita();
         partita.Gioca();
     }
