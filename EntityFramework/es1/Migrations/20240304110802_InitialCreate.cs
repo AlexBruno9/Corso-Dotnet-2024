30a1d74b047fdb9bfe8583228b57b403d151f021
Alessandro <alessandrobruno199@gmail.com>
2024-03-04 15:16:36 +0100

    EFrameW

---
+++ EntityFramework/es1/Migrations/20240304110802_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace es1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clienti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cognome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cell = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clienti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prodotti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Prezzo = table.Column<double>(type: "float", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prodotti", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prodotti_Clienti_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clienti",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prodotti_ClienteId",
+                table: "Prodotti",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prodotti");
+
+            migrationBuilder.DropTable(
+                name: "Clienti");
+        }
+    }
+}
