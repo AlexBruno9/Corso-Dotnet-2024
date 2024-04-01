a07d3a4086190e093cf5774ed6e9bd63549555e1
unknown <alessandrobruno199@gmail.com>
2024-04-01 19:03:09 +0200

    Modifica 8.1

--- Razor/WebAppBrani/Pages/RimuoviBrano.cshtml.cs
+++ Razor/WebAppBrani/Pages/RimuoviBrano.cshtml.cs
@@ -5,25 +5,25 @@ using Newtonsoft.Json;
 
 namespace WebAppBrani.Pages
 {
-    public class RimuoviDaBraniModel : PageModel
+    public class RimuoviBranoModel : PageModel
     {
 
-        public Brano? Brano {get;set;}
+        public Brano? Brano { get; set; }
 
-     
 
-        public void OnGet(int id) 
+
+        public void OnGet(int id)
         {
             var json = System.IO.File.ReadAllText("wwwroot/json/Brani.json");
             var brani = JsonConvert.DeserializeObject<List<Brano>>(json)!;
-            Brano=brani.FirstOrDefault(p=>p.Id==id);
+            Brano = brani.FirstOrDefault(p => p.Id == id);
         }
 
         public IActionResult OnPost(int id) // ELIMINA IL BRANO DAL CATALOGO (FILE JSON)
         {
             var json = System.IO.File.ReadAllText("wwwroot/json/Brani.json");
             var brani = JsonConvert.DeserializeObject<List<Brano>>(json)!;
-            var list = brani.FirstOrDefault(p=>p.Id==id);
+            var list = brani.FirstOrDefault(p => p.Id == id);
             brani.Remove(list!);
 
             System.IO.File.WriteAllText("wwwroot/json/Brani.json", JsonConvert.SerializeObject(brani, Formatting.Indented));
