6c56bdc2204c4478c42e3c1dfd39769b0c543472
Alessandro <alessandrobruno199@gmail.com>
2024-04-08 12:53:44 +0200

    Brani MVCprova

---
+++ IdentityFramework/BraniMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BraniMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+// using BraniMVC.Models;
+// namespace BraniMVC.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    //se necessario, puoi aggiungere DbSet per altri modelli qui
+}
